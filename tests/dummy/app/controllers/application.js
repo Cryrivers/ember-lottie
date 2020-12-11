bde218916f44c593f5ccf65f39c1d50dd2a7d6f0
Eelke Feenstra <dev@eelke.net>
2020-12-11 10:27:12 +0100

    Improve demo
    Signed-off-by: Eelke Feenstra <dev@eelke.net>

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,15 +1,25 @@
-import Controller from '@ember/controller';
+import Controller from "@ember/controller";
 
 export default Controller.extend({
+  currentTime: 0,
+  totalTime: 0,
+
   actions: {
     animationHandler(animation) {
       this.animation = animation;
+      this.animation.addEventListener("enterFrame", (evt) => {
+        this.set("totalTime", evt.totalTime.toFixed(3));
+        this.set("currentTime", evt.currentTime.toFixed(3));
+      });
     },
     play() {
       this.animation && this.animation.play();
     },
     pause() {
       this.animation && this.animation.pause();
-    }
-  }
+    },
+    rewind() {
+      this.animation && this.animation.goToAndPlay(0, true);
+    },
+  },
 });
